75b6b6303cdd2d75332a4e86c7cc152025b40de9
Benjamin Price <price.bj@gmail.com>
2021-02-11 19:13:37 -0600

    fixed selectors

--- src/Components/CatSelector.js
+++ src/Components/CatSelector.js
@@ -12,63 +12,63 @@ const CatSelector = ({catChange}) => {
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='career' id='career'></input>
-      <label htmlFor="animal">Career</label>
+      <label htmlFor="career">Career</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='celebrity' id='celebrity'></input>
-      <label htmlFor="animal">Celebrity</label>
+      <label htmlFor="celebrity">Celebrity</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='dev' id='dev'></input>
-      <label htmlFor="animal">Dev</label>
+      <label htmlFor="dev">Dev</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='explicit' id='explicit'></input>
-      <label htmlFor="animal">Explicit</label>
+      <label htmlFor="explicit">Explicit</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='fashion' id='fashion'></input>
-      <label htmlFor="animal">Fashion</label>
+      <label htmlFor="fashion">Fashion</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='food' id='food'></input>
-      <label htmlFor="animal">Food</label>
+      <label htmlFor="food">Food</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='history' id='history'></input>
-      <label htmlFor="animal">History</label>
+      <label htmlFor="history">History</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='money' id='money'></input>
-      <label htmlFor="animal">Money</label>
+      <label htmlFor="money">Money</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='movie' id='movie'></input>
-      <label htmlFor="animal">Movie</label>
+      <label htmlFor="movie">Movie</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='music' id='music'></input>
-      <label htmlFor="animal">Music</label>
+      <label htmlFor="music">Music</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='political' id='political'></input>
-      <label htmlFor="animal">Political</label>
+      <label htmlFor="political">Political</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='religion' id='religion'></input>
-      <label htmlFor="animal">Religion</label>
+      <label htmlFor="religion">Religion</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='science' id='science'></input>
-      <label htmlFor="animal">Science</label>
+      <label htmlFor="science">Science</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='sport' id='sport'></input>
-      <label htmlFor="animal">Sport</label>
+      <label htmlFor="sport">Sport</label>
     </div>
     <div>
       <input onChange={catChange} type='radio' name='cat' value='travel' id='travel'></input>
-      <label htmlFor="animal">Travel</label>
+      <label htmlFor="travel">Travel</label>
     </div>
     </div>
     </div>
